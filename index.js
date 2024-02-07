85af0d4c3d08393cffa74b20ba5330763ad9c9f4
milushh <154788726+milushh@users.noreply.github.com>
2024-02-07 14:42:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+
+
+function m2(){
+   let mi = document.createElement('div');
+   mi.innerText = "Субхана-Ллахи ва би-хамди-хи";
+   mi.id = "mi"
+  
+
+   document.body.append( mi )
+   }
+   
+
+
+
+//    let milush = [{
+//       ss : "Субхана-Ллахи ва би-хамди-хи",
+//       s2: "Лфффффф"
+//    }]
+// function uy(){
+
+//    for(let i = 0; i < milush.length; i++){
+//       let div = document.createElement("div")
+//       div.innerText = milush[0].ss
+
+
+//    }
+// }
+
